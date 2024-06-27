be75d76794fbd2b4c3f96e7a8349386db70fd652
Fernando_Machaca <fernando.machaca@unmsm.edu.pe>
2024-06-26 20:38:40 -0500

    se añadio AddCategory

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const AddCategory = () => {
+  const [inputValue, setInputValue] = useState("Futbol");
+  const onInputChange = ({ target }) => {
+    //setInputValue("Hola mundo");
+    setInputValue(target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+      <input
+        type="text"
+        placeholder="Buscar gif"
+        value={inputValue}
+        onChange={onInputChange}
+        //onChange={(event) => onInputChange(event)}
+      />
+    </form>
+  );
+};
