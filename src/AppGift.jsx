7aa723e3c9a5835ab131ec2c5f1d9afad636d665
Fernando_Machaca <fernando.machaca@unmsm.edu.pe>
2024-06-27 00:42:40 -0500

    Se agrego una validacion de categorias

--- src/AppGift.jsx
+++ src/AppGift.jsx
@@ -1,25 +1,42 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 export const AppGift = () => {
   //espacio de memoria para almacenar categorias
   const [categories, setCategories] = useState(["Futbol", "Basquet"]);
   //Inicializamos con  [] -> arreglo y como ejemplo el futbol
   console.log("categories");
 
-  const AddCategory = () => {
-    console.log("Voley");
+  const onAddCategory = (newCategory) => {
+    console.log(newCategory);
     //DESUSTRUCTURACION
     //metodo 1
-    setCategories([...categories, "Voley"]);
+    // setCategories([...categories, "Voley"]);
     // metodo 2
     // setCategories((cat) => [...categories, "voley"]);
+    //setCategories({ newCategory, ...categories }); XERROR por poner llaves
+    //metodo 1 validacion no tan estricto
+    //    if (categories.includes(newCategory)) return;
+    // metodo3 validacion extircta con minusculas y mayusculas
+    if (
+      !categories.findIndex(
+        (category) => category.toLowerCase() === newCategory.toLowerCase()
+      )
+    )
+      return;
+
+    setCategories([newCategory, ...categories]);
   };
   return (
     <>
       {/*ol -> order list      li -> items*/}
       {/*Titulo*/}
       <div>AppGif</div>;{/*Impus*/}
+      <AddCategory
+        //setCategoryy={setCategories}
+        onNewValue={(value) => onAddCategory(value)}
+      />
+      {/*<button onClick={onAddCategory}>Agregar</button>*/}
       {/*Listado de gif*/}
-      <button onClick={AddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-export const AddCategory = () => {
-  const [inputValue, setInputValue] = useState("Futbol");
+export const AddCategory = ({ onNewValue }) => {
+  const [inputValue, setInputValue] = useState("");
   const onInputChange = ({ target }) => {
     //setInputValue("Hola mundo");
     setInputValue(target.value);
   };
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(inputValue);
+    // console.log(inputValue);
+    if (inputValue.trim().length <= 2) return;
+    // setCategoryy((categories) => [inputValue, ...categories]);
+    onNewValue(inputValue.trim());
+    setInputValue("");
   };
   return (
-    <form onSubmit={(event) => onSubmit(event)}>
+    // <form onSubmit={(event) => onSubmit(event)}>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="Buscar gif"
         value={inputValue}
         onChange={onInputChange}
+        // value={inputValue || ""}
         //onChange={(event) => onInputChange(event)}
       />
     </form>
   );
+};
+AddCategory.propTypes = {
+  onNewValue: PropTypes.func.isRequired,
 };
