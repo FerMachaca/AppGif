5f10bcd0972ed8dc03151301fc1ac1b07e5e1a59
Fernando_Machaca <fernando.machaca@unmsm.edu.pe>
2024-06-26 19:12:09 -0500

    Creacion de boton agregar categoria y creacion de lista de categorias

--- src/AppGift.jsx
+++ src/AppGift.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 export const AppGift = () => {
+  //espacio de memoria para almacenar categorias
+  const [categories, setCategories] = useState(["Futbol", "Basquet"]);
+  //Inicializamos con  [] -> arreglo y como ejemplo el futbol
+  console.log("categories");
+
+  const AddCategory = () => {
+    console.log("Voley");
+    //DESUSTRUCTURACION
+    //metodo 1
+    setCategories([...categories, "Voley"]);
+    // metodo 2
+    // setCategories((cat) => [...categories, "voley"]);
+  };
   return (
     <>
+      {/*ol -> order list      li -> items*/}
       {/*Titulo*/}
-      <div>AppGif</div>;{/*Titulo*/}
+      <div>AppGif</div>;{/*Impus*/}
+      {/*Listado de gif*/}
+      <button onClick={AddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
